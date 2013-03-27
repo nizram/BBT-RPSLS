dbd2aa5c94c0b8f3140b6c2b3438d4cb9594d318
Dan Marzin <dmarzin@gmail.com>
2013-03-27 18:31:15 -0400

    complete one run version

--- rpsls.py
+++ rpsls.py
@@ -4,18 +4,18 @@
 This is a simulator of the game Rock-Paper_Scissors-Lizard-Spock
 from The Big Bang Theory
 
-The rules of Rock-paper-scissors-lizard-Spock are:
+The rules of Rock-Paper-Scissors-Lizard-Spock are:
 
-    Scissors cut paper
-    Paper covers rock
-    Rock crushes lizard
+    Scissors cut Paper
+    Paper covers Rock
+    Rock crushes Lizard
     Lizard poisons Spock
-    Spock smashes (or melts) scissors
-    Scissors decapitate lizard
-    Lizard eats paper
+    Spock smashes Scissors
+    Scissors decapitate Lizard
+    Lizard eats Paper
     Paper disproves Spock
-    Spock vaporizes rock
-    Rock breaks scissors
+    Spock vaporizes Rock
+    Rock breaks Scissors
 
 """
 import random
@@ -33,15 +33,68 @@ def selectUserThrow():
    print "\t3: Scissors"
    print "\t4: Lizard"
    print "\t5: Spock"
+   selection = raw_input("Selection: ")
+   return list[int(selection)-1]
 
-   #return (selection)
-
-def determineResults(computer, user):
-    if computer == user:
-        return "tie"
+def determineResults(computer, player):
+    if computer == "rock":
+        if player == "rock":
+            print "Rock vs Rock results in a tie"
+        elif player == "paper":
+            print "Paper covers Rock.  The player wins!!"
+        elif player == "scissors":
+            print "Rock breaks Scissors.  The computer wins!!"
+        elif player == "lizard":
+            print "Rock crushes Lizard.  The computer wins!!"
+        elif player == "spock":
+            print "Spock vaporizes Rock.  The player wins!!"
+    elif computer == "paper":
+        if player == "rock":
+            print "Paper covers Rock.  The computer wins!!"
+        elif player == "paper":
+            print "Paper vs Paper results in a tie"
+        elif player == "scissors":
+            print "Scissors cut Paper.  The player wins!!"
+        elif player == "lizard":
+            print "Lizard eats Paper.  The player wins!!"
+        elif player == "spock":
+            print "Paper disproves Spock.  The computer wins!!"
+    elif computer == "scissors":
+        if player == "rock":
+            print "Rock breaks Scissors.  The player wins!!"
+        elif player == "paper":
+            print "Scissors cut Paper.  The computer wins!!"
+        elif player == "scissors":
+            print "Scissors vs Scissors results in a tie"
+        elif player == "lizard":
+            print "Scissors decapitate Lizard.  The computer wins!!"
+        elif player == "spock":
+            print "Spock smashes Scissors.  The player wins!!"
+    elif computer == "lizard":
+        if player == "rock":
+            print "Rock crushes Lizard.  The player wins!!"
+        elif player == "paper":
+            print "Lizard eats Paper.  The computer wins!!"
+        elif player == "scissors":
+            print "Scissors decapitate Lizard. The player wins!!"
+        elif player == "lizard":
+            print "Lizard vs Lizard results in a tie"
+        elif player == "spock":
+            print "Lizard poisons Spock.  The computer wins!!"
+    elif computer == "spock":
+        if player == "rock":
+            print "Spock vaporizes Rock.  The computer wins!!"
+        elif player == "paper":
+            print "Paper disproves Spock. The player wins!!"
+        elif player == "scissors":
+            print "Spock smashes Scissors.  The computer wins!!"
+        elif player == "lizard":
+            print "Lizard poisons Spock.  The player wins!!"
+        elif player == "spock":
+            print "Spock vs Spock results in a tie"
     else:
         print "Something broke... exiting!!!"
     #exit with error
     
 if __name__ == "__main__":
-    selectUserThrow()
+    determineResults(selectComputerThrow(),selectUserThrow())
